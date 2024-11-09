146eafe35050e3b9acfc93253eb600d82d9a5a3a
Storm-py <ranamuneebmay2018@gmail.com>
2024-11-09 15:29:44 +0500

    solved some errors

--- src/app/(pages)/services/page.jsx
+++ src/app/(pages)/services/page.jsx
@@ -17,7 +17,7 @@ const ServicesPage = () => {
   useEffect(() => {
     const fetchServices = async () => {
       try {
-        const response = await fetch(`${SERVER_URL}/api/create-service`, {
+        const response = await fetch(`${process.env.SERVER_URL}/api/create-service`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
